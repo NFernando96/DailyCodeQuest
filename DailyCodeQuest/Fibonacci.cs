d463f1188d5c38fd07ba932d99e89f20e15a6d08
FERNANDO K.B.R.N <kbrnfernando96@outlook.com>
2023-09-09 21:38:37 +0530

    Add project files.

---
+++ DailyCodeQuest/Fibonacci.cs
@@ -0,0 +1,55 @@
+﻿namespace DailyCodeQuest;
+
+internal class Fibonacci
+{
+    public static void PrintFibonacci()
+    {
+        int input;
+        do
+        {
+            Console.Write("Please enter a positive number: ");
+        } while (!int.TryParse(Console.ReadLine(), out input) || input <= 0);
+
+        var fibonacciSeries = GetFibonacci(input);
+
+        if (fibonacciSeries != null)
+        {
+            for (int i = 0; i < fibonacciSeries.Count; i++)
+            {
+                Console.Write($"Term {i + 1} -> {fibonacciSeries[i]} , ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Unable to generate Fibonacci series.");
+        }
+    }
+
+    private static List<int>? GetFibonacci(int input)
+    {
+        try
+        {
+            if (input <= 0)
+            {
+                return new List<int>() { 0 };
+            }
+
+            List<int> fibonacciSeries = new List<int>() { 0, 1 };
+
+            for (int i = 2; i < input; i++)
+            {
+                checked
+                {
+                    fibonacciSeries.Add(fibonacciSeries[^1] + fibonacciSeries[^2]);
+                }
+            }
+
+            return fibonacciSeries;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Input is too large. Please enter a smaller number.");
+            return null;
+        }
+    }
+}
---
+++ DailyCodeQuest/Program.cs
@@ -0,0 +1,9 @@
+﻿using DailyCodeQuest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Fibonacci.PrintFibonacci();
+    }
+}
