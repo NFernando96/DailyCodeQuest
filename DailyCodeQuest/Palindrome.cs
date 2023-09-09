2574c7705fdb250eb98111de25a7ce3f9cd7f576
FERNANDO K.B.R.N <kbrnfernando96@outlook.com>
2023-09-09 23:38:50 +0530

    Palindrome Checker

---
+++ DailyCodeQuest/Palindrome.cs
@@ -0,0 +1,45 @@
+﻿namespace DailyCodeQuest
+{
+    internal class Palindrome
+    {
+        public static void CheckPalindrome()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine()!;
+
+            string formattedString = input.Replace(" ", "").ToLower();
+
+            var isPalindrome = IsPalindrome(formattedString);
+
+            if (isPalindrome)
+            {
+                Console.WriteLine($""" "{input}" is a Palindrome :)""");
+            }
+            else
+            {
+                Console.WriteLine($""" "{input}" is not a Palindrome :(""");
+            }
+
+
+        }
+
+        private static bool IsPalindrome(string input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
--- DailyCodeQuest/Program.cs
+++ DailyCodeQuest/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Fibonacci.PrintFibonacci();
+        // Fibonacci.PrintFibonacci();
+        Palindrome.CheckPalindrome();
     }
 }
